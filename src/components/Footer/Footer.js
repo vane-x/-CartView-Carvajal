74f7d9bd4853bcf75884533abb60d7f8f4677eec
vane-x <100387855+vane-x@users.noreply.github.com>
2022-08-18 02:42:11 -0400

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer (){
+
+  return(
+    <footer>
+      <div className="footer--contenido">
+          <div className="footer--informacion">
+              <h3>runClub</h3>
+              <p className="footer--texto">Pagina de prueba</p>
+              <p className="footer--texto">Curso de Coderhouse</p>
+          </div>
+          <div className="footer--redes">
+
+
+          </div>
+      </div>
+    </footer>
+  )
+}
